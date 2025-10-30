c9e657b73d717f6d28d4f541113eb5f6788b4d65
Mohamed <mohamedkondombo135@gmail.com>
2025-10-29 20:18:21 -0400

    Implement Navbar & Main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Bookings from "./pages/Bookings";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
+import Cabins from "./pages/Cabins";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
@@ -19,6 +20,7 @@ const App = () => {
             <Route index element={<Navigate replace to='dashboard' />} />
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
             <Route path='users' element={<Users />} />
             <Route path='settings' element={<Settings />} />
             <Route path='account' element={<Account />} />
---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+// import {
+//   HiOutlineCalendar,
+//   HiOutlineCog6Tooth,
+//   HiOutlineHome,
+//   HiOutlineHomeModern,
+//   HiOutlineUsers,
+// } from "react-icons/hi2";
+
+// const NavLinks = [
+//   {
+//     label: "Home",
+//     path: "/",
+//     icon: <HiOutlineHome />,
+//   },
+//   {
+//     label: "Bookings",
+//     path: "/bookings",
+//     icon: <HiOutlineCalendar />,
+//   },
+//   {
+//     label: "Cabins",
+//     path: "/cabins",
+
+//     icon: <HiOutlineHomeModern />,
+//   },
+//   {
+//     label: "Users",
+//     path: "/users",
+//     icon: <HiOutlineUsers />,
+//   },
+//   {
+//     label: "Settings",
+//     path: "/settings",
+//     icon: <HiOutlineCog6Tooth />,
+//   },
+// ];
+
+// export { NavLinks };
